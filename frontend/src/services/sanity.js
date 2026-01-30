09e0c97ce618148cb4c4c39046783ee262ee4817
Linda Jensen <linda.jensen81@me.com>
2026-01-30 16:40:41 +0100

    Update Sanity

--- frontend/src/services/sanity.js
+++ frontend/src/services/sanity.js
@@ -4,10 +4,8 @@ import { createImageUrlBuilder } from "@sanity/image-url";
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
-  // useCdn: false,
   useCdn: true,
   apiVersion: "2024-01-01",
-  // token: import.meta.env.VITE_SANITY_READ_TOKEN,
   perspective: "published",
 });
 
